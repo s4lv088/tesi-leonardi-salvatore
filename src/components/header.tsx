51617e263708d1756b51ab9bb35d27d9e03603a9
LAPTOP-1F8UL3S0\Salvatore <leonsalva@libero.it>
2024-05-07 12:42:37 +0200

    cleaning code

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,38 +1,35 @@
-import { cn } from '@/lib/utils';
-import imgLogo from '../assets/react.svg';
-import { Menu, X } from 'lucide-react';
-import { MobileSidebar } from './layout/mobile-sidebar';
+import { cn } from "@/lib/utils";
+import { Menu, X } from "lucide-react";
+import { MobileSidebar } from "./layout/mobile-sidebar";
 
 type HeaderProps = {
-    toggleSidebar: () => void;
-    sidebarVisible: boolean;
+  toggleSidebar: () => void;
+  sidebarVisible: boolean;
 };
 export function Header({ toggleSidebar, sidebarVisible }: HeaderProps) {
-  
-
-    return (
-        <div className="flex items-center justify-between border-b py-1 h-[60px]">
-            <div className="flex gap-4 ml-4 items-center">
-                <div className={cn('block lg:!hidden')}>{<MobileSidebar />}</div>
-                <X
-                    size={32}
-                    onClick={toggleSidebar}
-                    className={cn(
-                        'rotate-90 scale-100 transition-all duration-200 cursor-pointer from-gray-800 dark:from-gray-100',
-                        !sidebarVisible && ' -rotate-90 scale-0',
-                        'hidden lg:block'
-                    )}
-                />
-                <Menu
-                    size={32}
-                    onClick={toggleSidebar}
-                    className={cn(
-                        'rotate-360 absolute scale-100 transition-all duration-200 cursor-pointer from-gray-800 dark:from-gray-100',
-                        sidebarVisible && ' rotate-0 scale-0',
-                        'hidden lg:block'
-                    )}
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-between border-b py-1 h-[60px]">
+      <div className="flex gap-4 ml-4 items-center">
+        <div className={cn("block lg:!hidden")}>{<MobileSidebar />}</div>
+        <X
+          size={32}
+          onClick={toggleSidebar}
+          className={cn(
+            "rotate-90 scale-100 transition-all duration-200 cursor-pointer from-gray-800 dark:from-gray-100",
+            !sidebarVisible && " -rotate-90 scale-0",
+            "hidden lg:block"
+          )}
+        />
+        <Menu
+          size={32}
+          onClick={toggleSidebar}
+          className={cn(
+            "rotate-360 absolute scale-100 transition-all duration-200 cursor-pointer from-gray-800 dark:from-gray-100",
+            sidebarVisible && " rotate-0 scale-0",
+            "hidden lg:block"
+          )}
+        />
+      </div>
+    </div>
+  );
 }
--- src/components/layout/nav-items.tsx
+++ src/components/layout/nav-items.tsx
@@ -1,80 +1,56 @@
-'use client';
+"use client";
 
-import { MENU_ITEMS, MenuItem } from '@/lib/Constants';
-import { NavLink, useMatch } from 'react-router-dom';
-import clsx from 'clsx';
-import { useEffect, useRef, useState } from 'react';
+import { MENU_ITEMS, MenuItem } from "@/lib/Constants";
+import { NavLink } from "react-router-dom";
+import clsx from "clsx";
 
 interface NavItemsProps {
-    closeMobileSidebar?: () => void;
+  closeMobileSidebar?: () => void;
 }
 
 function MenuItemComponent({
-    item,
-    className = '',
-    onClick,
+  item,
+  className = "",
+  onClick,
 }: {
-    item: MenuItem;
-    className?: string;
-    onClick?: () => void;
+  item: MenuItem;
+  className?: string;
+  onClick?: () => void;
 }) {
-    const routeParent = useMatch(item.url + '/*');
-    const isActiveParent = !!routeParent;
+  const Icon = item.icon;
+  const handleClickNavLink = () => {
+    if (!item.emptyPage) {
+      onClick && onClick();
+    }
+  };
 
-    const route = useMatch(item.url);
-    const isActive = !!route;
-
-    const [height, setHeight] = useState(0);
-    const ref = useRef<HTMLDivElement>(null);
-
-    //Gestisce l'animazione dell'altezza del submenu
-    useEffect(() => {
-        const resizeObserver = new ResizeObserver((entry) => {
-            setHeight(isActiveParent ? entry[0].contentRect.height : 0);
-        });
-        if (ref?.current) {
-            resizeObserver.observe(ref.current);
-        }
-        return () => {
-            resizeObserver.disconnect();
-        };
-    }, [ref.current, isActiveParent]);
-
-    const Icon = item.icon;
-
-    const handleClickNavLink = () => {
-        if ( !item.emptyPage) {
-            onClick && onClick();
-        }
-    };
-
-    return (
-        <>
-           
-                <NavLink
-                    to={item.url}
-                    onClick={handleClickNavLink}
-                    className={clsx(
-                        className,
-                        `flex items-center px-6 py-4 font-base text-lg w-full transition-transform`,
-                       
-                    )}
-                >
-                    {Icon && <Icon className="mr-4 size-6" />}
-                    {item.label}
-                </NavLink>
-                
-            
-        </>
-    );
+  return (
+    <>
+      <NavLink
+        to={item.url}
+        onClick={handleClickNavLink}
+        className={clsx(
+          className,
+          `flex items-center px-6 py-4 font-base text-lg w-full transition-transform`
+        )}
+      >
+        {Icon && <Icon className="mr-4 size-6" />}
+        {item.label}
+      </NavLink>
+    </>
+  );
 }
 
 export function NavItems({ closeMobileSidebar }: NavItemsProps) {
-    return (
-        <nav className="grid items-start">
-            {MENU_ITEMS.map((item) => (
-                <MenuItemComponent item={item} onClick={closeMobileSidebar} key={item.url} />
-            ))}
-        </nav>
-    );
+  return (
+    <nav className="grid items-start">
+      {MENU_ITEMS.map((item) => (
+        <MenuItemComponent
+          item={item}
+          onClick={closeMobileSidebar}
+          key={item.url}
+        />
+      ))}
+    </nav>
+  );
 }
